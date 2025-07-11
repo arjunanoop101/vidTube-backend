fa9b1d98d5faffa5e6f4fadd19c9fd72a154167b
arjunanoop101 <arjunanoop101@gmail.com>
2025-07-11 13:05:59 +0530

    healthcheck route added

--- src/app.js
+++ src/app.js
@@ -12,4 +12,10 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+//routes
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthcheckRouter);
+
 export { app };
